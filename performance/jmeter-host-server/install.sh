df0ab1ad7a0f1fd37078c3eb271194a057c4afde
Eduardo Mioto <miotojava@gmail.com>
2020-08-11 20:16:40 -0300

    update jmeter
    update jmeter

--- performance/jmeter-host-server/install.sh
+++ performance/jmeter-host-server/install.sh
@@ -1,10 +1,15 @@
 yum update -y
-yum install java-1.8.0-openjdk
-java -version
-wget http://apache.stu.edu.tw//jmeter/binaries/apache-jmeter-5.2.1.tgz
-tar -xf apache-jmeter-5.2.1.tgz
+sudo yum install java-1.8.0-openjdk
+sudo java -version
+sudo wget http://apache.stu.edu.tw//jmeter/binaries/apache-jmeter-5.2.1.tgz
+sudo tar -xf apache-jmeter-5.2.1.tgz
+mv apache-jmeter-5.2.1 jmeter
+mv jmeter /opt/jmeter
 
 #vim ~/.bashrc
-#export JMETER_HOME=/root/apache-jmeter-5.2.1
+#export JMETER_HOME=/opt/jmeter
 #export PATH=$JMETER_HOME/bin:$PATH
 #source ~/.bashrc
+
+sudo chmod a+rx /opt/jmeter/bin/jmeter.sh
+sudo cp /opt/jmeter/bin/jmeter.sh /usr/local/bin
--- performance/observability/install.sh
+++ performance/observability/install.sh
@@ -1,3 +1,5 @@
+## On CentOS / Fedora
+
 sudo yum install -y yum-utils \
   device-mapper-persistent-data \
   lvm2
@@ -8,6 +10,19 @@ sudo yum-config-manager \
 
 sudo yum install docker-ce
 sudo usermod -aG docker $(whoami)
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+========================
+## On AWS Linux
+
+sudo yum update -y
+sudo yum install -y docker
+sudo usermod -aG docker ec2-user
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 
